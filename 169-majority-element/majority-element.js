e89b70c6591d313cebe8f0b3b65243d0b0af4baf
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2024-02-01 12:13:47 +0530

    Time: 67 ms (28.67%) | Memory: 55.1 MB (5.09%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let mp = new Map();
+    for (const n of nums) {
+        if (!mp.has(n)) {
+            mp.set(n, 0)
+        }
+        mp.set(n, mp.get(n) + 1)
+    }
+    let maxValue = -1;
+    let maxKey = -1;
+    for (const [key, value] of mp.entries()) {
+        console.log(key, value)
+        if (value > maxValue) { maxValue = value; maxKey = key; }
+    }
+    return maxKey
+};
